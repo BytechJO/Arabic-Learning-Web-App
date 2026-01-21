213224b7d012cd57db49396cf0605292458041e8
duha1dahamsheh <developer2@bytechjo.com>
2026-01-21 11:51:11 +0300

    solve

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,14 +25,7 @@ const progressRouter = require("./routes/progress.routers");
 // import googleAuthRoutes from "./routes/googleAuth.js";
 const app = express();
 
-app.use(cors({
-  origin: "/*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
-
-// مهم: معالجة preflight
-app.options(/.*/, cors());
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
